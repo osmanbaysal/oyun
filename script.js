d96dd898ffd43f6536b969adad4493c56154b3f5
osmanbaysal <99905428+osmanbaysal@users.noreply.github.com>
2024-05-20 22:07:08 +0300

    aaaa

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const shortcuts = [
     { keys: "Control+z", description: "Geri al" },
     { keys: "Control+y", description: "İleri al" },
     { keys: "Control+a", description: "Tümünü Seç" },
-    { keys: "Control+p", description: "Yazdır" },
-    { keys: "Control+s", description: "Kaydet" }
 ];
 
 let currentShortcutIndex = 0;
